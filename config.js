051c01ff683409ec3d93531cae3304ae4dbca5dc
小城故事 <68381226+web-xc@users.noreply.github.com>
2022-10-09 21:28:12 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,36 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'XC-Blog状态监控',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm792828406-27c64b6a638cfa8841fee671',
+    'm792828406-27c64b6a638cfa8841fee671',
+    'm792828406-27c64b6a638cfa8841fee671',
+    'm792828406-27c64b6a638cfa8841fee671',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://status.org.cn/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/yb/uptime-status'
+    },
+    {
+      text: 'Blog',
+      url: 'https://abo.xyz/'
+    },
+  ],
+};
