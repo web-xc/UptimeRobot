6cfd8b64127b50b7c8835448f219a75f81fd3749
小城故事 <68381226+web-xc@users.noreply.github.com>
2024-04-20 00:21:58 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ window.Config = {
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
     'm792828406-27c64b6a638cfa8841fee671',
+    'm792828406-11837e59bbcf13058d27b0da',
     'm792828485-2bea7c047611a8c17bb8c941',
     'm792828547-2e8da6457debc9318889d52d',
     'm792828490-fd28d5796981f5bc0ffe45c6',
@@ -27,15 +28,15 @@ window.Config = {
   Navi: [
     {
       text: '首页',
-      url: 'http://xc.webxc.ml/'
+      url: 'https://blog3.webxc.eu.org/'
     },
     {
       text: '博客',
-      url: 'https://www.webxc.ml/'
+      url: 'https://webxc.eu.org/'
     },
     {
       text: '友人帐',
-      url: 'https://www.webxc.ml/link/'
+      url: 'https://webxc.eu.org/link/'
     },
   ],
 };
